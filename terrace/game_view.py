bf10be41236851fcfdba6e2d1e15c0ecde97067b
Ana Cristina Ramos <up201904969@fe.up.pt>
2024-02-21 23:55:17 +0000

    Update board visualization

--- terrace/game_view.py
+++ terrace/game_view.py
@@ -4,7 +4,12 @@ class GameView:
     def __init__(self, model):
         self.model = model
         pygame.init()
-        self.window = pygame.display.set_mode((800, 800))
+
+        self.window_size = 860
+        self.margin = 30
+
+        self.window = pygame.display.set_mode((self.window_size, self.window_size))
+
         pygame.display.set_caption("Terrace Game (LEIC-IA Group 112)")
 
     def draw(self):
@@ -12,17 +17,28 @@ class GameView:
         self.window.fill((255, 255, 255))
 
         # Define 8 distinct, moderately saturated colors for the L-shapes
-        colors = [(255, 79, 134), (211, 105, 156), (189, 122, 173), (158, 131, 184), (128, 126, 184), (108, 149, 208), (93, 173, 233), (81, 203, 255)]
+        colors = [(81, 203, 255),
+                  (93, 173, 233),
+                  (108, 149, 208),
+                  (128, 126, 184),
+                  (158, 131, 184),
+                  (189, 122, 173),
+                  (211, 105, 156),
+                  (255, 79, 134)]
 
         # Draw the grid
         for i in range(8):
             for j in range(8):
                 # Select color based on cell position to create L-shaped pattern
                 color = colors[max(min(i, j), min(7-i, 7-j))]
-                pygame.draw.rect(self.window, color, (i*100, j*100, 100, 100))
+                pygame.draw.rect(self.window, color, (self.margin + i*100, self.margin + j*100, 100, 100))
 
                 # Draw border around each cell
-                pygame.draw.rect(self.window, (0, 0, 0), (i*100, j*100, 100, 100), 1)
+                pygame.draw.rect(self.window, (0, 0, 0), (self.margin + i*100, self.margin + j*100, 100, 100), 1)
+
+
+        # Draw a thicker border around the entire board
+        pygame.draw.rect(self.window, (0, 0, 0), (self.margin, self.margin, 800, 800), 5)
 
         # Update the display
         pygame.display.update()
